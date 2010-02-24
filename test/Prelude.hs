2e617ac85ffd48c760e49ce6e59bbb9df95ada18
xanxys <xanxys@gmail.com>
2010-02-24 11:12:50 +0900

    *Re-arranged Prelude.hs
    Ignore-this: 8232ff9d662dcd439f551873e5b5a3b4
    
    darcs-hash:20100224021250-09b00-51d0fb953bd7081048fa99aa5224fbccb6938725

--- test/Prelude.hs
+++ test/Prelude.hs
@@ -1,41 +1,130 @@
 
+-- internal
+seq=undefined
+undefined=undefined
+addByteRaw=undefined
+subByteRaw=undefined
+cmpByteRaw=undefined
+
+
+-- generic combinanors
+infixr 9 .
+(.) f g x=f (g x)
+
 infixr 0 $
 f $ x=f x
 
+infixr 0 $!
 f $! x=x `seq` (f x)
 
+
+id x=x
+flip f x y=f y x
+
+
+-- boolean
 data Bool
     =False
     |True
 
+infixr 2 ||
+x || y=
+    case x of
+        True  -> True
+        False -> y
+
+infixr 3 &&
+x && y=
+    case x of
+        False -> False
+        True  -> y
+
+otherwise=True
+
+
+-- maybe
+data Maybe a
+    =Nothing
+    |Just a
+
+
+-- either
+data Either a b
+    =Left a
+    |Right b
+
+
+-- ordering
 data Ordering
     =EQ -- 0
     |LT -- 1
     |GT -- 2
 
+
+-- tuple
 data XT1 a=XT1 a
 data XT2 a b=XT2 a b
 data XT3 a b c=XT3 a b c
 
+
+
+-- list
 data XList a
     =XCons a (XList a)
     |XNil
 
+
+head (x:xs)=x
+tail (x:xs)=xs
+
+reverse []=[]
+reverse (x:xs)=reverse xs++[x]
+
+map f []=[]
+map f (x:xs)=f x:map f xs
+
+filter f []=[]
+filter f (x:xs)
+    |f x       = x:filter f xs
+    |otherwise = filter f xs
+
+(x:xs) !! n
+    |n `eqByte` 0 = x
+    |otherwise    = xs !! (n `subByte` 1)
+
+xs ++ ys=
+    case xs of
+        []   -> ys
+        x:xs -> x:(xs++ys)
+
+
+{- I don't know why, but this code doesn't work!
+[]++ys=ys
+(x:xs)++ys=x:(xs++ys)
+-}
+
+
+
+length []=0
+length (x:xs)=1 `addByte` (length xs)
+
+foldr f z []=z
+foldr f z (x:xs)=f x (foldr f z xs)
+
+foldl f z []=z
+foldl f z (x:xs)=foldl f (f x z) xs
+
+
+-- I/O
 data E
     =Input (Char -> E)
     |Output !Char E
     |Halt
 
-otherwise=True
-
---seq x y=y
-seq=undefined
-undefined=undefined
 
 
-addByteRaw=undefined
-subByteRaw=undefined
-cmpByteRaw=undefined
+
+
 
 addByte x y=x `seq` (y `seq` (addByteRaw x y))
 subByte x y=x `seq` (y `seq` (subByteRaw x y))
@@ -82,42 +171,4 @@ addInt (NInt x) (PInt y)
 -}
 
 
--- list functions
-head (x:xs)=x
-tail (x:xs)=xs
-
-reverse []=[]
-reverse (x:xs)=reverse xs++[x]
-
-map f []=[]
-map f (x:xs)=f x:map f xs
-
-filter f []=[]
-filter f (x:xs)
-    |f x       = x:filter f xs
-    |otherwise = filter f xs
-
-(x:xs) !! n
-    |n `eqByte` 0 = x
-    |otherwise    = xs !! (n `subByte` 1)
-
-{- I don't know why, but this code doesn't work!
-[]++ys=ys
-(x:xs)++ys=x:(xs++ys)
--}
-
-xs ++ ys=
-    case xs of
-        []   -> ys
-        x:xs -> x:(xs++ys)
-    
-
-length []=0
-length (x:xs)=1 `addByte` (length xs)
-
-foldr f z []=z
-foldr f z (x:xs)=f x (foldr f z xs)
-
-foldl f z []=z
-foldl f z (x:xs)=foldl f (f x z) xs
 
