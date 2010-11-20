6636b8a4aa02e14de93e9a3c563d8046fc67fcbc
xanxys <xanxys@gmail.com>
2010-11-20 13:48:51 +0900

    fixed version display
    Ignore-this: 6b587362b07976a5e73c7c5f9617001a
    
    darcs-hash:20101120044851-09b00-f324a3ec3d849c0eb3eb5d9198c423e7916068cc

--- Main.hs
+++ Main.hs
@@ -122,7 +122,7 @@ partialChain opt from (c0,c1,g0,g1,s0,s1,b)=do
         LangBF       -> b  bf
 
 version :: String
-version="Haskell to Brainf**k Compiler: version 0.5"
+version="Haskell to Brainfuck Compiler: version 0.6.2"
 
 help :: String
 help=unlines $
